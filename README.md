ce14fb665071e7a089a89fa6f9734b8850ee1641
Robert Saunders <rtsaunders19@gmail.com>
2017-05-23 01:47:03 -0500

    MVP styling almost finished

---
+++ README.md
@@ -0,0 +1,3 @@
+APIKEY=AIzaSyB1JA9JKjQ3hL4QmifylNZLrKONntTU5QE
+
+https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
--- index.ios.js
+++ index.ios.js
@@ -21,60 +21,69 @@ export default class recycleApp extends Component {
 
     this.state = {
       num: 1,
-      chemical: 'PETE',
-      recycleable: 'Yes'
+      chemical: 'PET (Polyethylene Terephthalate)',
+      recycleable: 'YES',
+      description: 'PET is one of the most commonly used plastics in consumer products, and is found in most water and pop bottles, and some packaging. It is intended for single use applications; repeated use increases the risk of leaching and bacterial growth.'
     };
   }
 
-  chemical(value) {
+
+  onSlide(value) {
     switch (value) {
       case 1:
         this.setState({
           num: 1,
-          chemical: 'PETE',
-          recycleable: 'Yes'
+          chemical: 'PET (Polyethylene Terephthalate)',
+          recycleable: 'YES',
+          description: 'PET is one of the most commonly used plastics in consumer products, and is found in most water and pop bottles, and some packaging. It is intended for single use applications; repeated use increases the risk of leaching and bacterial growth.'
         })
         break;
       case 2:
         this.setState({
           num: 2,
-          chemical: 'HDPE',
-          recycleable: 'Yes'
+          chemical: 'HDPE (High-Density Polyethylene)',
+          recycleable: 'YES',
+          description: 'HDPE is a stiff plastic used to make picnic tables, plastic lumber, waste bins, park benches, bed liners for trucks and other products which require durability and weather-resistance. Products made of HDPE are reusable and recyclable.'
         })
         break;
       case 3:
         this.setState({
           num: 3,
-          chemical: 'V',
-          recycleable: 'No'
+          chemical: 'PVC (Polyvinyl Chloride)',
+          recycleable: 'NO',
+          description: 'PVC is dubbed the “poison plastic” because it contains numerous toxins which it can leach throughout its entire life cycle.  Because PVC is relatively impervious to sunlight and weather, it is used to make window frames, garden hoses, arbors, raised beds and trellises. Products made using PVC plastic are not recyclable. '
         })
         break;
       case 4:
         this.setState({
           num: 4,
-          chemical: 'LDPE',
-          recycleable: 'Yes'
+          chemical: 'LDPE (Low-Density Polyethylene)',
+          recycleable: 'YES',
+          description: 'It is not commonly recycled, however, this is changing in many communities today as more recycling programs gear up to handle this material like the city of Austin. When recycled, LDPE plastic is used for plastic lumber, landscaping boards, garbage can liners and floor tiles.'
         })
         break;
       case 5:
         this.setState({
           num: 5,
-          chemical: 'PP',
-          recycleable: 'Yes'
+          chemical: 'PP (Polypropylene)',
+          recycleable: 'YES',
+          description: 'Polypropylene is recyclable through some curbside recycling programs, but only about 3% of PP products are currently being recycled in the US. Recycled PP is used to make landscaping border stripping, battery cases, brooms, bins and trays. However, #5 plastic is today becoming more accepted by recyclers.'
         })
         break;
       case 6:
         this.setState({
           num: 6,
-          chemical: 'PS',
-          recycleable: 'No'
+          chemical: 'PS (Polystyrene)',
+          recycleable: 'NO',
+          description: 'Polystyrene is an inexpensive, lightweight and easily-formed plastic with a wide variety of uses. It is most often used to make disposable styrofoam drinking cups, take-out “clamshell” food containers, egg cartons, plastic picnic cutlery, foam packaging and those ubiquitous “peanut” foam chips used to fill shipping boxes to protect the contents.'
         })
         break;
       case 7:
         this.setState({
           num: 7,
-          chemical: 'Other',
-          recycleable: 'No'
+          chemical: 'Other (BPA, Polycarbonate and LEXAN)',
+          recycleable: 'NO',
+          description: '#7 plastics are not for reuse, unless they have the PLA compostable coding. When possible it is best to avoid #7 plastics, especially for children’s food. Plastics with the recycling labels #1, #2 and #4 on the bottom are safer choices and do not contain BPA.'
         })
     }
   }
@@ -82,7 +91,7 @@ export default class recycleApp extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: '#00A5CF' }}>
 
       <View style={styles.headerStyle}>
         <Text style={styles.headerText}>Can I Recycle This?</Text>
@@ -90,26 +99,30 @@ export default class recycleApp extends Component {
 
       <View style={styles.logoArea}>
         <Text style={styles.name}>{this.state.chemical}</Text>
-        <Image style={styles.image} />
-        <Text style={styles.name}>{this.state.num}</Text>
+        <Image style={styles.image} source={{ uri: 'https://openclipart.org/image/2400px/svg_to_png/196353/recycle-plastic.png' }}>
+          <Text style={styles.number}>{this.state.num}</Text>
+        </Image>
       </View>
 
       <View style={styles.mainSection}>
         <Text style={styles.mainHeader}>Recycleable?</Text>
-        <Text style={styles.mainHeader}>{this.state.recycleable}</Text>
-        <Image style={styles.box} />
-        <Text style={styles.description}>lorem empsum jdnd loofdiue ibujek hdvikhbeiw
-        jasbd hbsjdhb hasjnf hbssvve</Text>
+        <View style={styles.box} >
+          <Text style={{ width: 30, height: 30, color: 'black' }}>{this.state.recycleable}</Text>
+        </View>
+        <View style={{ justifyContent: 'center', alignItems: 'center', paddingTop: 30, height: 40 }}>
+          <Text style={styles.description}>{this.state.description}</Text>
+        </View>
       </View>
 
       <View style={styles.slider}>
         <Slider
+          minimumTrackTintColor={'white'}
           style={styles.slider}
           value={this.state.num}
           minimumValue={1}
           maximumValue={7}
           step={1}
-          onValueChange={newValue => this.chemical(newValue)}
+          onValueChange={newValue => this.onSlide(newValue)}
         />
       </View>
 
@@ -124,37 +137,50 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     height: 60,
     paddingTop: 25,
-    backgroundColor: '#8BC34A'
+    backgroundColor: '#8BC34A',
+    color: 'white'
   },
   headerText: {
-    fontSize: 20
+    fontSize: 20,
+    color: 'white'
   },
   logoArea: {
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingTop: 15
   },
   name: {
-    fontSize: 15
+    fontSize: 15,
+    color: 'white'
+  },
+  number: {
+    fontSize: 15,
+    color: 'white',
   },
   image: {
-
+    height: 100,
+    width: 100,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   mainSection: {
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    height: 200
   },
   mainHeader: {
-
+    color: 'white'
   },
   box: {
-
+    backgroundColor: 'white',
+    height: 30
   },
   description: {
-
+    color: 'white',
+    fontSize: 10
   },
   slider: {
     justifyContent: 'center',
-    flex: 1
   }
 });
 
