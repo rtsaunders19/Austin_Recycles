8fc14278d18eacdeaab90f78c4807400b98b5f75
Robert Saunders <rtsaunders19@gmail.com>
2017-05-23 14:15:22 -0500

    final changes before I put it into components

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import Achievements from './components/Achievements';
+
+const RouterComponent = () => {
+  return (
+    <Router sceneStyle={{ paddingTop: 65 }}>
+      <Scene key="achievements" component={Achievements} title="Your Achievements"/>
+    </Router>
+  );
+};
+
+export default RouterComponent;
---
+++ src/components/Achievements.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+const Achievements = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+};
+
+export default Achievements;
